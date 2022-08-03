27141727cb5d7267f7e0dbd754b95bab6a9eecb5
Seiko Santana <santanaaseiko@gmail.com>
2022-08-03 13:02:51 +0700

    Added Pop shell shortcut script

---
+++ pop_22_04_shell_shortcut.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Super>1']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Super>2']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Super>3']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Super>4']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Super>5']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Super>6']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Super>7']"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super>8']"
